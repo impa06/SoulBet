3e9285cbbe8bd6b9bd3bb40eccc53bc2a00cd40f
Tortuga-X <juanandresbetancur2006@gmail.com>
2025-11-14 16:56:58 -0500

    se agrega la opcion de cambiar el volumen

---
+++ Assets/Scripts/Volumen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volumen : MonoBehaviour
+{
+    public Slider slider;
+    public float sliderValue;
+
+    void Start()
+    {
+        slider.value=PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume=slider.value;
+    }
+
+    public void changeSlider(float valor){
+        sliderValue=valor;
+        PlayerPrefs.SetFloat("volumenAudio", sliderValue);
+        AudioListener.volume=slider.value;
+    }
+}
+    
